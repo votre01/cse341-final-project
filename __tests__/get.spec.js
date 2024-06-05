a30e76426fa7cd4261a328ae71151c638e65bf61
Trevor <ngw21001@byui.edu>
2024-06-05 20:01:44 +0200

    tests jest

--- __tests__/get.spec.js
+++ __tests__/get.spec.js
@@ -29,7 +29,7 @@ describe('Test Handlers', () => {
 
     test('responds to /clients', async () => {
         const res = await request.get('/clients');
-        expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+        expect(res[0].header['content-type']).toBe('text/html; charset=utf-8');
         expect(res.statusCode).toBe(200)
     })
 })
